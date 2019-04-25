9f09f3c013f78bba29087f71db53c8b0e15b36c6
liuqitoday <liuqitoday@163.com>
2019-04-25 19:33:31 +0800

    problem100

---
+++ src/Problem100.java
@@ -0,0 +1,31 @@
+/**
+ * https://leetcode-cn.com/problems/same-tree/submissions/
+ */
+public class Problem100 {
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == null && q == null) {
+            return true;
+        }
+        if (p != null && q != null && p.val == q.val) {
+            return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+        } else {
+            return false;
+        }
+    }
+
+    public  void main(String[] args) {
+
+    }
+
+}
